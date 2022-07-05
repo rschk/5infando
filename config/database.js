5e1a10e36352862775d1a6cef27f0e3413278594
unknown <marianareischakpereira@gmail.com>
2022-07-05 17:59:20 -0300

    first commit

---
+++ config/database.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+
+const uri =
+  "mongodb+srv://rschk:<colorada8>@maricluster.gzsmp.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose.connect(uri);
+
+module.exports = mongoose;
